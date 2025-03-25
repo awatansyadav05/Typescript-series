27e57f84b47cbbfaafbca58c29fa72fb51961000
Awatans Yadav <theavi0509@gmail.com>
2025-03-25 23:41:44 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+// this is the app.ts file converted into the 'js' file when we compile the 'ts' file it will be created 'js' file automatically
+var a;
---
+++ app.ts
@@ -0,0 +1,2 @@
+// this is the app.ts file converted into the 'js' file when we compile the 'ts' file it will be created 'js' file automatically
+var a;
